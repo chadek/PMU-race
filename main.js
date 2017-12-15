47f9d99e9ceee5d0644784fc2495d4cca52c6ec1
Myro <olivier.meyer973@gmail.com>
2017-12-16 00:49:09 +0100

    added JS number of players with animations

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 
-var playersNumberSelector 	= document.querySelector("#players-number");
-var playersNumber 			= playersNumberSelector.options[playersNumberSelector.selectedIndex].value;
-playersNumberSelector.addEventListener('change', changePlayerNumber);
+/*
+règles de syntaxe :
+- tous les noms de variables en camelCase
+- ajouter Selector derrière le nom de la variable quand celle cie est un élément du DOM
+*/
+
+
+var playerNumberSelector 	= document.querySelector("#player-number");
+var playerNumber 			= playerNumberSelector.options[playerNumberSelector.selectedIndex].value;
+playerNumberSelector.addEventListener('change', changePlayerNumber);
+
+var playerHolderSelector = document.querySelector("#players-holder");
 
 function changePlayerNumber() {
-	playersNumber = playersNumberSelector.options[playersNumberSelector.selectedIndex].value;
+	playerNumber = playerNumberSelector.options[playerNumberSelector.selectedIndex].value;
+	
+	for(var i=0; i<8; i++) {
+		if (i < playerNumber) {
+			playerHolderSelector.children[i].classList.remove("hidden");
+			playerHolderSelector.children[i].classList.remove("pickDown");
+		} else {
+			playerHolderSelector.children[i].classList.add("pickDown");
+			hideMenuPlayer(i);
+		}
+	}
 }
 
+function hideMenuPlayer(i) {
+	//cache les lignes dont on n'a pas besoin avec un délai correspondant au temps de l'animation
+	setTimeout(function () {
+		playerHolderSelector.children[i].classList.add("hidden");
+   	}, 700);			
+}
 
-console.log(playersNumber);
